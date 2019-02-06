b6d83ea54c930b42c4aa3131b52035185f0092a1
KotJazz <32486240+KotJazz@users.noreply.github.com>
2019-02-06 11:44:53 +0100

    Update main.c

--- automatecellulaire/main.c
+++ automatecellulaire/main.c
@@ -8,15 +8,13 @@ int main()
 {
     enum cell buffer[SIZE][SIZE];//Buffer array to save the previous grid state.
     enum cell grid[SIZE][SIZE];//Modify the SIZE constant in order to modify the size of the plane.
-    int cptgen=1,i;
-    printf("How many generations would you like to see ?");
     emptyfile();
     init(buffer);
     display(buffer);
     do
     {
         printf("\n");
-        system("pause"); //Passage à la génération suivante
+        system("pause"); //Passage Ã  la gÃ©nÃ©ration suivante
         system("cls");
         generation(buffer,grid,cptgen);
         display(grid);
